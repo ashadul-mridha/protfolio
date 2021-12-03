62b3e513824cc24ba7e2ce1188c2cd4b933a61f0
ashadul-mridha <ashadulislam708@gmail.com>
2021-12-03 20:59:45 +0600

    header done

--- src/Components/Pages/Shared/Header/Header.js
+++ src/Components/Pages/Shared/Header/Header.js
@@ -1,19 +1,22 @@
 import React from 'react';
+import './Header.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <div>
-           <div className="logo">
-               <h1>hello Mam </h1>
-            </div>
-            <nav>
-                <ul>
-                    <li>Service</li>
-                    <li>Protfolio</li>
-                    <li>Pricing</li>
-                </ul>
-            </nav>
-        </div>
+        <Navbar collapseOnSelect expand="lg" className="bg-main py-4" variant="light">
+            <Container>
+            <Navbar.Brand className="text-white fs-4 fw-bold"> ashadul.me </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="ms-auto">
+                    <Nav.Link href="#features" className="fs-5 text-white text-uppercase">Protfolio</Nav.Link>
+                    <Nav.Link href="#pricing" className="fs-5 text-white text-uppercase">Service</Nav.Link>
+                    <Nav.Link href="#pricing" className="fs-5 text-white text-uppercase">Pricing</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
