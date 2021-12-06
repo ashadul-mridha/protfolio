f7d333c2a62384248ddb8a1bef8c4e021da5798e
ashadul-mridha <ashadulislam708@gmail.com>
2021-12-06 12:40:24 +0600

    resume button added

--- src/Components/Home/HeroSection/HeroSection.js
+++ src/Components/Home/HeroSection/HeroSection.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './HeroSection.css';
+import resume from '../../../files/ashadul_islam-resume_ph.pdf';
 
 const HeroSection = () => {
     return (
@@ -7,7 +8,7 @@ const HeroSection = () => {
             <div className="hero-content">
                 <h2>I'm Full Stack Developer <br/> & App Developer</h2>
                 <p>Specializing in custom theme development. If you're a business <br/> seeking a web presence or are looking to hire, contact me here.</p>
-                <button className="btn btn-warning rounded">Get Started</button>
+                <a href={resume} download="ashadul-resume.pdf" ><button className="btn btn-warning rounded text-dark"><i class="fas fa-download"></i> Download Resume</button></a>
             </div>
         </div>
     );
